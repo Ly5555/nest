170c827c4daa0aa531887f2a18bbcf67bf202cec
liuyongqing <>
2023-06-26 20:41:50 +0800

    新增部分

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateBookDto {
     @ApiProperty({ description: '描述该条目的原始文本' })
+    @IsNotEmpty({ message: "书名不能为空" })
     title: string;
     @ApiProperty({ description: '作者' })
     author: string;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import {  NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AllResponseInterceptor } from './all-response.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { AnyExcptionFilter } from './any-excption.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 使用自定义全局拦截对数据做处理
+  app.useGlobalInterceptors(new AllResponseInterceptor)
+  app.useGlobalPipes(new ValidationPipe)
+  app.useGlobalFilters(new AnyExcptionFilter) // 对服务器异常统一处理
   const options = new DocumentBuilder()
     .setTitle('Cats example') // 标题 
     .setDescription('自动生成') // 描述
